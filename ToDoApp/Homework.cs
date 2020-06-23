56c49c9a6ad2fd05c8655177eae57bec990389ab
Dakamakat <gazon-ozon@yandex.ru>
2020-06-24 01:06:25 +0300

    feat: Added two new classes

---
+++ ToDoApp/Homework.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp
+{
+    class Homework
+    {
+        private string _subject;
+
+        private string _objective;
+
+        static int count = 0;
+
+        private List<Homework> homeworks = new List<Homework>(count);
+
+        public List<Homework> Homeworks
+        {
+            get
+            {
+                return Homeworks;
+            }
+            set
+            {
+                if (count == count + 1)
+                {
+                    homeworks.Add(new Homework());
+                }
+            }
+        }
+
+
+
+
+        public DayOfWeek DayOfCreation { get; }
+        
+        public Homework() { }
+        public Homework(string subject , string objective, DayOfWeek dayOfCreation)
+        {
+            count++;
+            this._subject = subject;
+            this._objective = objective;
+            this.DayOfCreation = dayOfCreation;
+            
+           
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Subject: {_subject}  Objective: {_objective} DateOfCreation {DayOfCreation}");
+            foreach(var a in homeworks)
+            {
+                Console.WriteLine(a);
+            }
+            
+        }
+
+    }
+}
--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ToDoApp
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {       
+
+            
         }
     }
 }
