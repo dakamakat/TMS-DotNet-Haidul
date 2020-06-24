126d7b4f854fbd9b821128b95092bcc7add26e96
Dakamakat <gazon-ozon@yandex.ru>
2020-06-24 04:25:02 +0300

    feat: Added some methods

--- ToDoApp/Homework.cs
+++ ToDoApp/Homework.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ToDoApp
 {
@@ -10,49 +9,66 @@ namespace ToDoApp
 
         private string _objective;
 
-        static int count = 0;
+        private readonly DayOfWeek dayOfCreation = DateTime.Now.DayOfWeek;
 
-        private List<Homework> homeworks = new List<Homework>(count);
-
-        public List<Homework> Homeworks
-        {
-            get
-            {
-                return Homeworks;
-            }
-            set
-            {
-                if (count == count + 1)
-                {
-                    homeworks.Add(new Homework());
-                }
-            }
-        }
+        List<Homework> listOfHomeworks { get; set; } = new List<Homework>();
 
 
 
-
-        public DayOfWeek DayOfCreation { get; }
-        
         public Homework() { }
         public Homework(string subject , string objective, DayOfWeek dayOfCreation)
+
         {
-            count++;
             this._subject = subject;
             this._objective = objective;
-            this.DayOfCreation = dayOfCreation;
+            this.dayOfCreation = dayOfCreation;
             
            
         }
-        public void GetInfo()
+        public void GetInfo(Homework homework)
         {
-            Console.WriteLine($"Subject: {_subject}  Objective: {_objective} DateOfCreation {DayOfCreation}");
-            foreach(var a in homeworks)
+            foreach (var goal in homework.listOfHomeworks)
             {
-                Console.WriteLine(a);
+                Console.WriteLine($"Subject: {goal._subject}  Objective: {goal._objective} DayOfCreation {goal.dayOfCreation}");
+            }
+
+        }
+        public void AddHomework(Homework homework)
+        {
+            Console.WriteLine("Enter the number of homeworks that you wanna add");
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < input; i++)
+                {
+                    Homework goal = new Homework(Console.ReadLine(), Console.ReadLine(), DateTime.Now.DayOfWeek);
+                    homework.listOfHomeworks.Add(goal);
+        }
+
+    }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a number");
+            }
+        }
+        public void DeleteHomework(Homework homework)
+        {
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (homework.listOfHomeworks != null)
+                {
+                    homework.listOfHomeworks.RemoveAt(input);
+                }
+                else
+                    Console.WriteLine("There is no homeworks");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Enter a number of homework which y wanna delete");
+            
             }
             
         }
-
     }
 }
