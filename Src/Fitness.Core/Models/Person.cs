4f7ddb1ddaebaf29388af0d53802c77f03f73702
Dakamakat <gazon-ozon@yandex.ru>
2020-07-08 21:35:27 +0300

    feat:Add new class

---
+++ Src/Fitness.Core/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness.Core.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int Age { get; set; }
+        public IList<Result> CaloriesPerDay { get; set; } = new List<Result>();
+    }
+}
---
+++ Src/Fitness.Core/Models/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness.Core.Models
+{
+    public class Result
+    {
+        public DateTime Date { get; set; }
+        public double Calories { get; set; }
+    }
+}
